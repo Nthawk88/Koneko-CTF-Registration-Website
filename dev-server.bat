68d644eedcc3f7f83bc3cf0130dae66d6cdbf68a
nthawk <darnantas@gmail.com>
2025-10-16 08:43:02 +0700

    Clean up testing and development files
    - Remove testing avatar files from uploads/avatars/ directory
    - Remove development scripts (dev-server.bat, start.sh)
    - Keep uploads directory structure for production use
    - Maintain all core functionality and website appearance

--- dev-server.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting CTF Registration Website Development Server...
-echo.
-echo Website will be available at: http://localhost:9000
-echo.
-echo Press Ctrl+C to stop the server
-echo.
-docker-compose up --build
